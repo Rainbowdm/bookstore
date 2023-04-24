bc8a8dc723cd9bd7a547f93b18d474d890dcbef9
Rainbowdm <dim8035@gmail.com>
2023-04-24 17:24:29 +0300

    05_023 documentation Swagger

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,3 +1,8 @@
 JW_SECRET_KEY = "96173b4221d1bf4f9630cdfec9878d191db89239e9dd228a90959b405b904502"
 JWT_ALGORITH = "HS256"
 JWT_EXPIRATION_TIME_MINUTES = 60 * 24 * 5
+
+TOKEN_DESCRIPTION = "It checks username and password. If they are true, it returns JWT token."
+TOKEN_SUMMARY = "It returns JWT token"
+
+ISBN_DESCRIPTION = "It is unique identifier for books"
