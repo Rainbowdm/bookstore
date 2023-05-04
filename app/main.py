b68e0d8c9e6ec92668f7126da1fa4445063794c4
Rainbowdm <dim8035@gmail.com>
2023-05-04 23:16:50 +0300

    07_32 deploying API to prod env

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
 
-from utils.constants import (TOKEN_DESCRIPTION, TOKEN_SUMMARY, REDIS_URL, TESTING,
-                             IS_PRODUCTION, REDIS_URL_PRODUCTION)
+from app.utils.constants import (TOKEN_DESCRIPTION, TOKEN_SUMMARY, REDIS_URL, TESTING,
+                                 IS_PRODUCTION, REDIS_URL_PRODUCTION)
 from app.routes.v1 import app_v1
 from app.routes.v2 import app_v2
 from starlette.requests import Request
@@ -16,6 +16,7 @@ import app.utils.redis_object as r
 import aioredis
 from app.utils.redis_object import check_test_redis
 import pickle
+import logging
 
 app = FastAPI(title="Bookstore API documentation", description="API used for Bookstore", version="1.0.0")
 app.include_router(app_v1, prefix="/v1", dependencies=[Depends(check_jwt_token), Depends(check_test_redis)])
@@ -43,7 +44,7 @@ async def disconnect_db():
 @app.post("/token", description=TOKEN_DESCRIPTION, summary=TOKEN_SUMMARY)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     redis_key = f"token:{form_data.username}, {form_data.password}"
-    print("redis_key:" + redis_key)
+    logging.debug("redis_key:" + redis_key)
     user = await r.redis.get(redis_key)
     if not user:
         jwt_user_dict = {"username": form_data.username, "password": form_data.password}
@@ -75,7 +76,7 @@ async def middleware(request: Request, call_next):
     # modify response
     execution_time = (datetime.utcnow() - start_time).microseconds
     response.headers["x-execution-time"] = str(execution_time)
-    print("x-execution-time" + execution_time)
+    logging.debug("x-execution-time")
     return response
 
 
