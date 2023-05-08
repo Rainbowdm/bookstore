08a196d83b05515c0590547200b415ea77d1c581
Rainbowdm <dim8035@gmail.com>
2023-05-08 17:16:21 +0300

    08_035 Reverse Proxy

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,9 @@ ssh root@209.38.237.156 'docker container rm bookstore-api'
 
 ssh root@209.38.237.156 'docker build -t bookstore-api-build ~/bookstore'
 ssh root@209.38.237.156 'docker run --name=bookstore-api -d -e MODULE_NAME="main" -e PORT="8000" -e PRODUCTION="true" -p 8000:8000 bookstore-api-build'
+
+ssh root@209.38.237.156 'docker stop api-nginx'
+ssh root@209.38.237.156 'docker rm api-nginx'
+
+ssh root@209.38.237.156 'docker build -t bookstore-nginx ~/bookstore/nginx-reverse-proxy'
+ssh root@209.38.237.156 'docker run --name=api-nginx -d -p 80:80 bookstore-nginx'
